0903b725a8739599d055d032e01aad015b5cef31
dbqls200 <dbqls200@gmail.com>
2024-08-10 17:22:21 +0900

    chore: Initial project setup with Xcode

---
+++ Ae-shley/Ae-shley/Ae_shleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ae_shleyApp.swift
+//  Ae-shley
+//
+//  Created by 김유빈 on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Ae_shleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ae-shley/Ae-shley/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ae-shley
+//
+//  Created by 김유빈 on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
