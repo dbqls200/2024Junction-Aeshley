fb9cbd21124bc36d2550259318beb767e6c966e1
Seo-Jooyoung <tjwndud0412@naver.com>
2024-08-11 10:24:03 +0900

    [chore] 앱 foreground 상태일 때 알림 뜨게 수정

--- Ae-shley/Ae-shley/Ae_shleyApp.swift
+++ Ae-shley/Ae-shley/Ae_shleyApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct Ae_shleyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             NavigationView {
@@ -16,5 +17,21 @@ struct Ae_shleyApp: App {
             }
             .navigationBarBackButtonHidden()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    // Foreground(앱 켜진 상태)에서도 알림 오는 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
     }
 }
