e3004842f00e2ed1c3e5c268c2a9878ea4cec09d
Seo-Jooyoung <tjwndud0412@naver.com>
2024-08-11 10:40:14 +0900

    [refactor] 코드 정리

--- Ae-shley/Ae-shley/Ae_shleyApp.swift
+++ Ae-shley/Ae-shley/Ae_shleyApp.swift
@@ -31,7 +31,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 extension AppDelegate: UNUserNotificationCenterDelegate {
     
     // Foreground(앱 켜진 상태)에서도 알림 오는 설정
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, 
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.list, .banner])
     }
 }
